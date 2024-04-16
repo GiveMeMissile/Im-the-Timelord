ee44d859c04a8654f426f630727b7268f8e1a1f8
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-04-16 09:34:44 -0500

    Type.py better
    It dose something now

--- Type.py
+++ Type.py
@@ -1,14 +1,24 @@
 import time
 import random
-def ask(a):
+def asked(b):
+    input("You must rewrite the entire sentence below as quickley as possible. You will be timed and should try to type it as fast as possible with as little errors as possible. Press enter when you are ready. good luck.")
+    print(b)
+    start = time.time()
+    typed = input("Type the sentence above here: ")
+    end = time.time()
+    total = end - start
+    print(total)
+def sentence(a):
     match a:
         case 1:
-            print("s1")
+            b = "The Placeholder"
         case 2:
-            print("s2")
+            b = "Placeholder"
         case 3:
-            print("s3")
+            b = "Another Placeholder"
         case 4:
-            print("s4")
+            b = "Placeholder 2.0"
+    return b
 a = random.randint(1, 4)
-ask(a)
+b = sentence(a)
+asked(b)
