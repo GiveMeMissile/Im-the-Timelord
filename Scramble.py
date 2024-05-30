4b5b6c07d84f3172cc0ed0654e66d97fe956c681
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2024-05-30 10:38:46 -0500

    Finished Scramble.py
    I have finished this. Might update it later probably will not though as it seems to be fully finished.

--- Scramble.py
+++ Scramble.py
@@ -2,6 +2,7 @@ import random
 import time
 def intro():
   print("Hello and welcome to this fun word scrambling game. You will be given a word and you will have to unscramble it. You will be given points for every word you unscramble correctly. You will have to unscramble the word in under 10 seconds. You will be able to add your own words to the list. You must get 10 points to win. Good luck!")
+  print(" ")
   c = 0
   while(c == 0):
     custom = input("Would you like to add your own words to the list?: ")
@@ -12,26 +13,32 @@ def intro():
       c = 2
       return c
     else:
+      print(" ")
       print("Please enter yes or no")
+      print(" ")
 def custom(c):
   word1 = "The"
   word2 = "Power"
-  word3 = "Infinite"
+  word3 = "Everlasting"
   word4 = "Capitulation"
-  word5 = "Dictator"
+  word5 = "Eldritch"
   word6 = "Parasitic"
-  word7 = "Relations"
+  word7 = "Containment"
   if c == 1:
+    print(" ")
     print("You will begin to add your own words to the list. You will be asked how many words you wanna add. It can either be 1, 3, 5, or 7 words.")
     t = 0
     while t == 0:
+      print(" ")
       try:    
         num = int(input("How many words would you like to add?: "))
         if num == 1 or num == 3 or num == 5 or num == 7:
           t += 1
         else:
+          print(" ")
           print("The number must be 1, 3, 5, or 7")
       except:
+        print(" ")
         print("Please enter a number")
     if(num == 1):
       word1 = input("Enter your first word: ")
@@ -59,56 +66,55 @@ def custom(c):
   else:
     return word1, word2, word3, word4, word5, word6, word7
 def game(w1, w2, w3, w4, w5, w6, w7):
-  def chose(word1, word2, word3, word4, word5, word6, word7):
+  def chose(w1, w2, w3, w4, w5, w6, w7):
     x = random.randint(1, 7)
     match x:
       case 1:
-        word = word1
+        word = w1
       case 2:
-        word = word2
+        word = w2
       case 3:
-        word = word3
+        word = w3
       case 4:
-        word = word4
+        word = w4
       case 5:
-        word = word5
+        word = w5
       case 6:
-        word = word6
+        word = w6
       case 7:
-        word = word7
-    caword = join(word)
-    return caword
-def rand(word):
-  random.shuffle(word)
-  raword = join(word)
-  return raword
-  v = input("Click enter to start. When you start the timer will start on the first word. All of the words are %s, %s, %s, %s, %s, %s, and %s. Make sure to unscramble the word in under 10 seconds. Good luck!" % (w1, w2, w3, w4, w5, w6, w7))
-  word1 = list(w1)
-  word2 = list(w2)
-  word3 = list(w3)
-  word4 = list(w4)
-  word5 = list(w5)
-  word6 = list(w6)
-  word7 = list(w7)
+        word = w7
+    return word
+  def rand(word):
+    lword = list(word)
+    random.shuffle(lword)
+    raword = ''.join(lword)
+    return raword
+  print(" ")
+  input("Click enter to start. When you start the timer will start on the first word. All of the words are %s, %s, %s, %s, %s, %s, and %s. Make sure to unscramble the word in under 10 seconds. Good luck!" % (w1, w2, w3, w4, w5, w6, w7))
   points = 0
   attempts = 0
   while(points < 10):
+    print(" ")
     attempts += 1
-    word, = chose(word1, word2, word3, word4, word5, word6, word7)
+    word = chose(w1, w2, w3, w4, w5, w6, w7)
     raword = rand(word)
     begin = time.time()
     answer = input("Unscramble the word: %s : " % (raword))
     end = time.time()
     total = end - begin
     if(total > 10):
-      input("You took too long to unscramble the word. ): Press enter to continue")
+      print(" ")
+      input("You took too long to unscramble the word. You have %d points): Press enter to continue" % (points))
     elif(answer == word):
-      input("Correct the word was %s. Press enter to continue" % (word))
       points += 1
+      print(" ")
+      input("Correct the word was %s.You have %d points Press enter to continue" % (word, points))
     else:
-      input("Incorrect the word was %s. Press enter to continue" % (word))
+      print(" ")
+      input("Incorrect the word was %s.You have %d points Press enter to continue" % (word, points))
   return attempts
 c = intro()
 w1, w2, w3, w4, w5, w6, w7 = custom(c)
 attempts = game(w1, w2, w3, w4, w5, w6, w7)
+print(" ")
 print("You took %d attempts to get 10 points. Thank you for playing this game and have a WONDERFUL rest of your day :)." % (attempts))
